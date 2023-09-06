5e94edf86d259193cd83c91bad81f61cde8dfc7f
Mahakaal <116643805+CosmicDev4321@users.noreply.github.com>
2023-09-06 09:37:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+!python -m pip install pyyaml==5.1
+import sys, os, distutils.core
+!git clone 'https://github.com/facebookresearch/detectron2'
+dist = distutils.core.run_setup("./detectron2/setup.py")
+!python -m pip install {' '.join([f"'{x}'" for x in dist.install_requires])}
+sys.path.insert(0, os.path.abspath('./detectron2'))
+
+import torch, detectron2
+!nvcc --version
+TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
+CUDA_VERSION = torch.__version__.split("+")[-1]
+print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
+print("detectron2:", detectron2.__version__)
+
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+import numpy as np
+import os, json, cv2, random
+from google.colab.patches import cv2_imshow
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+cfg = get_cfg()
+cfg.MODEL.DEVICE = 'cpu'
+cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+predictor = DefaultPredictor(cfg)
+panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+cv2_imshow(out.get_image()[:, :, ::-1])
